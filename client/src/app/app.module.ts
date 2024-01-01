27dc61a3e45fbc7d8fff253760abcf3d04f53e0f
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2024-01-01 20:57:42 +0545

    world map

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {LogAnalyzerComponent} from "./home/log-analyzer/log-analyzer.component";
 import {LogAnalyzerService} from "./service/log-analyzer.service";
 import {JsonPipe} from "@angular/common";
 import {BrowserModule} from "@angular/platform-browser";
 import {HttpClientModule} from "@angular/common/http";
 import {NgApexchartsModule} from "ng-apexcharts";
+import {NgxEchartsModule} from "ngx-echarts";
+import * as echarts from 'echarts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,9 +22,12 @@ import {NgApexchartsModule} from "ng-apexcharts";
     AppRoutingModule,
     HttpClientModule,
     JsonPipe,
-    NgApexchartsModule
+    NgApexchartsModule,
+    NgxEchartsModule.forRoot({ echarts }),
+    NgbModule,
   ],
   providers: [LogAnalyzerService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
